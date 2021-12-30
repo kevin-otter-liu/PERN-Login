1203ba36ee288efcded1fa4c38d0d3f0edfe3c73
Kevin Liu <kevinliusingapore@gmail.com>
2021-12-31 01:55:35 +0800

    Added commit changes i forgot to commit

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ app.use(cors());
 //register and login ROUTES
 app.use('/auth', require('./routes/jwtAuth'));
 
+//register dashboard ROUTES
+app.use('/dashboard', require('./routes/dashboard'));
+
 app.listen(5000, () => {
   console.log('server is running on port 5000');
 });
--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -16,10 +16,11 @@ module.exports = async (req, res, next) => {
     // verify will return the payload which we can use as data to be passed
     // to the next function by attaching to the same request object
     const payload = jwt.verify(jwtToken, process.env.jwtSecret);
-
     req.user = payload.user;
   } catch (error) {
     console.error(error.message);
     return res.status(403).json('Not Authorized');
   }
+
+  next();
 };
